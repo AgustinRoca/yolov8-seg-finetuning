9a50121fd5a48d8ef96237a4316ee88cd0906cab
Agustín Roca <aroca@udesa.edu.ar>
2023-11-09 15:30:23 -0300

    Log deers found percentage

--- predict_single.py
+++ predict_single.py
@@ -3,17 +3,19 @@ import os
 from tqdm import tqdm
 
 def main():
-    # Load a model
     model = YOLO('runs/segment/train/weights/best.pt')
-
-    # Run batched inference on a list of images
 
     train_imgs = os.listdir('ground_truth')
     train_imgs = [img.split('_')[0] for img in train_imgs]
     imgs = os.listdir('imgs')
     imgs = [img for img in imgs if img.split('.')[0] not in train_imgs]
+    deers_found = 0
     for img in tqdm(imgs):
-        model.predict(os.path.join('imgs', img), save=True)
+        result = model.predict(os.path.join('imgs', img), save=True)
+        if result[0].masks:
+            deers_found += 1
+    print(f'Imagenes con venados encontrados: {deers_found} / {len(imgs)}')
+    print(f'Porcentaje del total: {deers_found / len(imgs) * 100:.2f}%')
 
 if __name__ == '__main__':
     main()
